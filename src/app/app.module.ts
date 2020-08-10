464fc0057c7799073cc251d59236eb24c13f2666
jamesonojae <jamesonojae@gmail.com>
2020-08-10 10:46:26 +0100

    Working on modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ElementsModule} from './elements/elements.module';
+import { CollectionsHomeComponent } from './collects/collections-home/collections-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CollectionsHomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ElementsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
